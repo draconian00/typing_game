7630f79e806557ab7aa306f40fa1b7ca9ddf5308
draconian <draconian@gmail.com>
2020-10-07 23:19:06 +0900

    complete view

--- src/views/Complete/index.ts
+++ src/views/Complete/index.ts
@@ -5,6 +5,14 @@ import Component from '@/views/base';
 import { IRouteParams } from '@/router';
 
 export default class CompleteView extends Component {
+  public avgTime: number = 0;
+  public totalScore: number = 0;
+  public totalTime: number = 0;
+  public successCount: number = 0;
+
+  public restartQuiz() {
+    this.$root.$router.back();
+  }
 
   // -------------------
   constructor(params?: IRouteParams) {
@@ -13,7 +21,24 @@ export default class CompleteView extends Component {
     this.$componentEl = document.createElement('div');
     this.$componentEl.id = 'complete_root';
     this.$componentEl.innerHTML = template;
-    console.log(params);
+
+    if (params) {
+      const { avgTime, totalScore, totalTime, successCount } = params;
+
+      if (avgTime && typeof avgTime === 'number') {
+        this.avgTime = avgTime;
+      }
+      if (totalScore && typeof totalScore === 'number') {
+        this.totalScore = totalScore;
+      }
+      if (totalTime && typeof totalTime === 'number') {
+        this.totalTime = totalTime;
+      }
+      if (successCount && typeof successCount === 'number') {
+        this.successCount = successCount;
+      }
+    }
+    
   }
 
   public mounted() {
